f7e3ab61e0dce6b93e427e4a6781f5d34b1360f4
Colin <73126555+Co1002@users.noreply.github.com>
2024-12-04 03:18:01 +0100

    Delete views directory

--- views/templates/footer.php
+++
@@ -1,9 +0,0 @@
-    </main>
-
-    <footer class="bg-primary-subtle">
-        <p class="mb-0 py-2 text-center text-secondary">© <?= date("Y") ?> - All rights reserved</p>
-    </footer>
-
-    <script src="assets/js/bootstrap.bundle.js"></script>
-</body>
-</html>
