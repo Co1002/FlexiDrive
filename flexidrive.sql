c922fe5485362d8f1743e27f3afd48b30af72636
Colin <73126555+Co1002@users.noreply.github.com>
2024-12-04 03:29:54 +0100

    Update and rename dbs13391471.sql to flexidrive.sql

--- flexidrive.sql
+++ flexidrive.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `dbs13391471`
+-- Datenbank: `flexidrive`
 --
 
 -- --------------------------------------------------------
